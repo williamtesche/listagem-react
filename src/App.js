248d80aee3df402536d1242833cd4ee04e661d44
Willian Matheus Ribeiro Tesche <85637800+williamtesche@users.noreply.github.com>
2021-11-26 16:32:27 -0300

    quase pronto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import './App.css';
-import TodoList from './components/TodoList';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <TodoList />
-    </div>
-  );
-}
+  let [lista, setLista] = useState(["tarefa 1", "tarefa 2"]);
+  let [novoItem,setNovoItem] = useState("");
 
+  return (
+    <div>
+      <h1>To Do List</h1>
+      <input value={novoItem} onChange={value => setNovoItem(value.target.value)} type="text" />
+      <button onClick={() => adicionarNovoItem()}>Adicionar</button>
+      <ul>
+        {lista.map((item,index) => (
+          <li>
+            {item}
+
+            <button onClick={() => deletarItem(index)}>
+              Deletar
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+
+
+
+  );
+  function adicionarNovoItem() {
+    setLista([...lista,novoItem])
+    setNovoItem("");
+  }
+
+  function deletarItem(index) {
+    let tmpArray = [...lista];
+    tmpArray.splice(index, 1);
+
+    setLista(tmpArray);
+  }
+  
+}
 export default App;
